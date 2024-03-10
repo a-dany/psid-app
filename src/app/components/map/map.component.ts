2d1529d493d765c471fa1b24da50764def9f49c0
a-dany <danyabouadaou@gmail.com>
2024-03-10 19:39:32 +0100

    smallfixs

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -43,8 +43,6 @@ export class MapComponent implements AfterViewInit, OnChanges {
     )
 
     // Init Tiles
-    // 'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png',
-    // 'http://sgx.geodatenzentrum.de/wmts_topplus_open/tile/1.0.0/web_grau/default/WEBMERCATOR/{z}/{y}/{x}.png',
     L.tileLayer( 
       'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
       {
@@ -57,16 +55,8 @@ export class MapComponent implements AfterViewInit, OnChanges {
 
   private initGeojson() {
     
-
     if (!this.geojson) return;
-    // L.geoJSON(this.geojson, {
-    //   style: {
-    //     color: '#FF000000', weight:1, fillColor:'red', fillOpacity:.1
-    //   }
-    // }).addTo(this.map)
 
-
-    // const data = this.geojson.features.map((e:any) => e.coordinates[0])
     let count=0;
     this.geojson.features.forEach(
       (e:any)=>{
@@ -78,33 +68,6 @@ export class MapComponent implements AfterViewInit, OnChanges {
         L.geoJSON(e, {style:style}).addTo(this.map)
       }
     )
-    // data.forEach((coords:any) => {
-    //   coords = coords.map((e:any)=>{return {lat:e[1],lon:e[0]}})
-    //   console.log(coords)
-    //   L.geoJSON(coords).addTo(this.map)
-    //   // L.polyline( coords).addTo(this.map)
-    //   // coords.forEach((e:any) => L.circleMarker(e).addTo(this.map))
-    // })
-
-    // let areas = this.geojson.filter((e:any) => e.tags.type === 'boundary').map((e:any) => e.members).flatMap((e:any) => e).filter((e:any) => e.type === 'way')
-    // let areas = this.geojson.filter((e:any) => e.tags.type === 'boundary').map((e:any) => e.members)//.flatMap((e:any) => e).filter((e:any) => e.type === 'way')
-
-    // areas.forEach((a:any) => {
-    //   let coords = areas[5].filter((e:any) => e.type === 'way').map((e:any) => e.geometry).flatMap((e:any) => e)
-    //   const poly = L.polygon(coords, {
-    //   })
-    //   coords.forEach((e:any) => {
-    //     // L.circleMarker(e).addTo(this.map)
-    //   })
-    //   const geojsonLayer = L.geoJSON(coords, {
-    //     style: function (feature) {
-    //         return { fillColor: 'red', fillOpacity: 0.5, color: 'red' };
-    //     }
-    // }).addTo(this.map);
-      // poly.setStyle({ fillColor: 'blue', fill:true })
-      // poly.addTo(this.map)
-    // })
-
 
   }
 
