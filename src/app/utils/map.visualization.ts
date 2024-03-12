9f39aa4112af8b3485c92ee5974c1b698b088708
a-dany <danyabouadaou@gmail.com>
2024-03-12 20:09:57 +0100

    map: pattern class (prices)

--- src/app/utils/map.visualization.ts
+++ src/app/utils/map.visualization.ts
@@ -1,31 +1,54 @@
-import * as L from 'leaflet';
 import { DataProviderService } from '../services/data-provider.service';
+import * as L from 'leaflet';
+import * as T from '@turf/turf';
 
 
 /***| MAIN ABSTRACT CLASS |***/
 
 export abstract class GeoData {
-    
+
+
+    /***| SHARED ATTRIBUTES |***/
+
     protected map!:L.Map;
 
     protected styleOuterBorders:L.PathOptions = { weight:2, color:'#00000045', fillOpacity:0 }
-    protected styleRegion:L.PathOptions = { color:'#00000060', weight:1, dashArray:"1 2" }
+    protected styleRegion:L.PathOptions = { color:'#00000030', weight:1, dashArray:"1 2" }
+
+
+    /***| ABSTRACT METHODS |***/
 
     public abstract display():void;
 
-    public defineMap(m:L.Map) { this.map = m }
+
+    /***| SHARED METHODS |***/
+
+    public defineMap(m:L.Map) { this.map = m 
+    }
     public clear() { console.log('Clear method not ready yet.');
+    }
+    protected borders(data:any) { L.geoJSON(data, {style: this.styleOuterBorders}).addTo(this.map)
     }
 
 }
+
+
 
 /***| PRICES |***/
 
 export class PricesGeoData extends GeoData {
 
-    constructor(private _provider:DataProviderService) { super();
+
+    /***| INIT |***/
+
+    private dataDistrictsLight:any;
+    constructor(private _provider:DataProviderService) { super(); this._provider.getDistrictColorMap().subscribe( ds => this.dataDistrictsLight = ds );
     }
-    public display() { this.data(); this.borders();
+
+
+    /***| ATTRIBUTES |***/
+
+    public display() { this.data(); // this._provider.getBordersRaw().subscribe( d => { this.borders(d) })
     }
 
     private data() {
@@ -34,63 +57,26 @@ export class PricesGeoData extends GeoData {
             if (!d) return;
 
             d.features.forEach((district:any) => {
-                let polygon = district.coordinates[0];
-                let opacity = .5;
-                this.styleRegion.fillColor = '#DD0000';
-                this.styleRegion.opacity = opacity;
+                
+                let polygon = T.polygon(district.coordinates)
+                let current = this.dataDistrictsLight.find((k:any) => T.booleanPointInPolygon([k.longitude, k.latitude] , polygon))
+
+                let color   = (current) ? "#DD0000" : 'gold';
+                let opacity = (current) ? parseFloat(current.mean_price) % 0.45 + 0.05 : 0.25;
+
+                this.styleRegion.fillColor   = color;
+                this.styleRegion.fillOpacity = opacity;
                 L.geoJSON(district, { style: this.styleRegion }).addTo(this.map)
+
             })
 
-        })
-    }
-    private borders() {
-        this._provider.getBordersRaw().subscribe( d => {
-            L.geoJSON(d, {style: this.styleOuterBorders}).addTo(this.map)
         })
     }
 
 }
 
-// private initGeojson() {
-    
-  //   if (!this.geojson) return;
-
-  //   this._data.getDistrictColorMap().subscribe(d => {
-
-  //     this.geojson.features.forEach(
-  //       (e:any)=>{
-
-  //         const polygon = T.polygon(e.coordinates)
-  //         let current = d.find((k:any) => T.booleanPointInPolygon([k.longitude, k.latitude] , polygon))
-
-  //         let color = "#DD0000"
-  //         if (!current) color = 'gold'
-  //         let opacity = (current) ? parseFloat(current.mean_price) % 0.6 + 0.15 : 0.5;
-  //         let style:L.PathOptions = {
-  //           color:'#00000060', fillColor:color, weight:1, fillOpacity:opacity,
-  //           dashArray:"1 2"
-  //         }
-  //         let layer = L.geoJSON(e, { style:style })
-
-  //         layer.addTo(this.map)
-  
-  //       }
-  //     )
-
-  //   })
-
-  //   this._data.getBordersRaw().subscribe(e => {
-  //     L.geoJSON(e, {style:{
-  //       weight:2, color:'#00000090', fillOpacity:0
-  //     }}).addTo(this.map)
-  //   })
-
-  // }
-
-
-  export class PopulationGeoData extends GeoData {
+export class PopulationGeoData extends GeoData {
     public display() {
         console.log('Population')
     }
-
 }
