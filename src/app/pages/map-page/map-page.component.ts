e179888c2e38a0f1bdaa4dd6afacdf11371d6a2b
a-dany <danyabouadaou@gmail.com>
2024-03-12 19:40:18 +0100

    map page refactoring

--- src/app/pages/map-page/map-page.component.ts
+++ src/app/pages/map-page/map-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataProviderService } from '../../services/data-provider.service';
+import { GeoData, PricesGeoData } from '../../utils/map.visualization';
 
 @Component({
   selector: 'app-map-page',
@@ -8,12 +9,20 @@ import { DataProviderService } from '../../services/data-provider.service';
 })
 export class MapPageComponent implements OnInit {
 
-  public rawDistricts!:any;
+  // public rawDistricts!:any;
+  public geodata!:GeoData;
+  
 
   constructor(private _data:DataProviderService) {}
+
   ngOnInit() {
-    this._data.getDistrictsRaw().subscribe( data => this.rawDistricts = JSON.parse(data)
-    )
+    // this._data.getDistrictsRaw().subscribe( data => this.rawDistricts = JSON.parse(data)
+    // ).add(() => {
+
+      let geo:GeoData = new PricesGeoData(this._data);
+      this.geodata = geo;
+
+    // })
   }
 
 }
